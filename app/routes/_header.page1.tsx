116c00e1bf8edb3535111448e8fb76c093cf8a9d
gforro <gabriel.forro@gmail.com>
2024-10-03 17:46:37 +0200

    styles

--- app/routes/_header.page1.tsx
+++ app/routes/_header.page1.tsx
@@ -1,50 +1,59 @@
-import { Link, useSubmit, useFetcher, Form } from '@remix-run/react';
-import { ActionFunctionArgs, MetaFunction, json } from '@remix-run/node';
-import { useEffect } from 'react';
+import {
+  Link,
+  useSubmit,
+  useFetcher,
+  Form,
+  useActionData,
+} from "@remix-run/react";
+import { ActionFunctionArgs, MetaFunction, json } from "@remix-run/node";
+import { useEffect } from "react";
 
 export async function loader() {
   await new Promise((r) => setTimeout(r, 200));
   return json({ ok: true });
 }
 
-export async function action({request}: ActionFunctionArgs) {
+export async function action({ request }: ActionFunctionArgs) {
   const data = await request.formData();
-  const duration = data.get('duration');
-  const timeout = typeof duration === 'string' ? Number(duration) : 2000;
+  const duration = data.get("duration");
+  const timeout = typeof duration === "string" ? Number(duration) : 2000;
   await new Promise((r) => setTimeout(r, timeout));
-  return json({ answer: 'pong', after: timeout });
+  return json({ answer: "pong", after: timeout });
 }
 
-
 export const meta: MetaFunction = () => {
-  return [{ title: 'Page 1' }];
+  return [{ title: "Page 1" }];
 };
 
 export default function Page1() {
-  const submit = useSubmit();
   const fetcher = useFetcher();
+  const fetcherForm = useFetcher();
 
   useEffect(() => {
-    if (fetcher.state === 'idle' && !fetcher.data) {
-      fetcher.load('/api/wait/20000');
+    if (fetcher.state === "idle" && !fetcher.data) {
+      fetcher.load("/api/wait/20000");
     }
   });
 
   return (
     <main className="flex flex-col gap-2 items-center">
       <h3 className="text-xl mb-5">Page 1</h3>
-      <span className="ext-sm text-gray-500">20 seconds: {fetcher.state}</span>
-      <Form method="post" className="flex items-end gap-2">
-      <label className="flex flex-col">
-        <span className="pl-2">Response time:</span>
-        <input type="number" name="duration" defaultValue={2000}></input>
-      </label>
-      <input type="submit"
-        className="bg-red-500 border-red-800 hover:bg-red-800 text-white px-8 py-1"
-        value="Submit"
-      />
-       
-      </Form>
+      <span className="ext-sm text-gray-500">
+        Fetcher in page1 (20 seconds response): {fetcher.state}
+      </span>
+      <fetcherForm.Form method="post" className="flex items-end gap-2">
+        <label className="flex flex-col">
+          <span className="pl-2">Response time:</span>
+          <input type="number" name="duration" defaultValue={2000}></input>
+        </label>
+        <input
+          type="submit"
+          className="bg-red-500 border-red-800 hover:bg-red-800 text-white px-8 py-1 hover:cursor-pointer"
+          value={
+            fetcherForm.state === "idle" ? "Submit" : `${fetcherForm.state}...`
+          }
+        />
+      </fetcherForm.Form>
       <Link className="hover:underline" to="/page2">
         Go to Page 2
       </Link>
--- app/routes/_header.tsx
+++ app/routes/_header.tsx
@@ -1,5 +1,5 @@
-import { Outlet, Link, useFetcher } from '@remix-run/react';
-import { useEffect } from 'react';
+import { Outlet, Link, useFetcher } from "@remix-run/react";
+import { useEffect } from "react";
 
 export default function Header() {
   const fetcher = useFetcher();
@@ -7,26 +7,35 @@ export default function Header() {
   const fetcher3 = useFetcher();
 
   useEffect(() => {
-    if (fetcher.state === 'idle' && !fetcher.data) {
-      fetcher.load('/api/wait/4000');
+    if (fetcher.state === "idle" && !fetcher.data) {
+      fetcher.load("/api/wait/4000");
     }
-    if (fetcher2.state === 'idle' && !fetcher2.data) {
-      fetcher2.load('/api/wait/6000');
+    if (fetcher2.state === "idle" && !fetcher2.data) {
+      fetcher2.load("/api/wait/6000");
     }
-    if (fetcher3.state === 'idle' && !fetcher3.data) {
-      fetcher3.load('/api/wait/8000');
+    if (fetcher3.state === "idle" && !fetcher3.data) {
+      fetcher3.load("/api/wait/8000");
     }
   }, [fetcher, fetcher2, fetcher3]);
 
   return (
-    <div className="flex flex-col gap-4 items-center">
+    <div className="flex flex-col gap-4 items-stretch">
+      <header className="flex flex-col items-center bg-red-100 pb-8">
       <h1 className="text-2xl">Header</h1>
-      <span className="text-sm text-gray-500">4 seconds: {fetcher.state}</span>
-      <span className="text-sm text-gray-500">6 seconds: {fetcher2.state}</span>
-      <span className="text-sm text-gray-500">8 seconds: {fetcher3.state}</span>
-      <div className="border px-10 py-4 border-gray-800">
-        <Outlet />
+      <div className="flex flex-col items-start">
+        <h5 className="text-xl">Fetchers in header with response times</h5>
+        <span className="text-sm text-gray-500">
+          4 seconds: {fetcher.state}
+        </span>
+        <span className="text-sm text-gray-500">
+          6 seconds: {fetcher2.state}
+        </span>
+        <span className="text-sm text-gray-500">
+          8 seconds: {fetcher3.state}
+        </span>
       </div>
+      </header>
+      <Outlet />
     </div>
   );
 }
