14e6bbdd3a44f39a2602ac5c875a6f795e9216b0
gforro <gabriel.forro@gmail.com>
2024-10-03 22:15:28 +0200

    small changes

--- app/routes/_header.page1.tsx
+++ app/routes/_header.page1.tsx
@@ -4,6 +4,7 @@ import {
   useFetcher,
   Form,
   useActionData,
+  useNavigation,
 } from "@remix-run/react";
 import { ActionFunctionArgs, MetaFunction, json } from "@remix-run/node";
 import { useEffect } from "react";
@@ -26,35 +27,55 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Page1() {
-  const fetcher = useFetcher();
+  // const fetcher = useFetcher();
   const fetcherForm = useFetcher();
 
-  useEffect(() => {
-    if (fetcher.state === "idle" && !fetcher.data) {
-      fetcher.load("/api/wait/20000");
-    }
-  });
+  // useEffect(() => {
+  //   if (fetcher.state === "idle" && !fetcher.data) {
+  //     fetcher.load("/api/wait/10000");
+  //   }
+  // }, [fetcher]);
 
   return (
-    <main className="flex flex-col gap-2 items-center">
-      <h3 className="text-xl mb-5">Page 1</h3>
-      <span className="ext-sm text-gray-500">
-        Fetcher in page1 (20 seconds response): {fetcher.state}
-      </span>
-      <fetcherForm.Form method="post" className="flex items-end gap-2">
-        <label className="flex flex-col">
-          <span className="pl-2">Response time:</span>
-          <input type="number" name="duration" defaultValue={2000}></input>
-        </label>
-        <input
-          type="submit"
-          className="bg-red-500 border-red-800 hover:bg-red-800 text-white px-8 py-1 hover:cursor-pointer"
-          value={
-            fetcherForm.state === "idle" ? "Submit" : `${fetcherForm.state}...`
-          }
-        />
+    <main className="flex flex-col gap-2 items-stretch px-8">
+      <h3 className="text-xl mb-5 text-center">Page 1</h3>
+      {/* <span className="ext-sm text-gray-500">
+        Fetcher in page1 (10 seconds response): {fetcher.state}
+      </span> */}
+      <fetcherForm.Form method="post">
+        <fieldset className="border border-solid border-gray-300 p-4 flex flex-col items-start gap-2">
+          <legend>Fetcher form (no navigation)</legend>
+          <label className="flex flex-col">
+            <span>Response time:</span>
+            <input type="number" name="duration" defaultValue={2000}></input>
+          </label>
+          <input
+            type="submit"
+            className="bg-red-500 border-red-800 hover:bg-red-800 text-white px-8 py-1 hover:cursor-pointer"
+            value={
+              fetcherForm.state === "idle"
+                ? "Submit"
+                : `${fetcherForm.state}...`
+            }
+          />
+        </fieldset>
       </fetcherForm.Form>
-      <Link className="hover:underline" to="/page2">
+
+      <Form method="post">
+        <fieldset className="border border-solid border-gray-300 p-4 flex flex-col items-start gap-2">
+          <legend>Form with navigation</legend>
+          <label className="flex flex-col">
+            <span>Response time:</span>
+            <input type="number" name="duration" defaultValue={2000}></input>
+          </label>
+          <input
+            type="submit"
+            className="bg-red-500 border-red-800 hover:bg-red-800 text-white px-8 py-1 hover:cursor-pointer"
+            value={"Submit"}
+          />
+        </fieldset>
+      </Form>
+      <Link className="hover:underline text-center mt-6 text-l" to="/page2">
         Go to Page 2
       </Link>
     </main>
--- app/routes/_header.tsx
+++ app/routes/_header.tsx
@@ -3,35 +3,20 @@ import { useEffect } from "react";
 
 export default function Header() {
   const fetcher = useFetcher();
-  const fetcher2 = useFetcher();
-  const fetcher3 = useFetcher();
-
+  
   useEffect(() => {
     if (fetcher.state === "idle" && !fetcher.data) {
       fetcher.load("/api/wait/4000");
     }
-    if (fetcher2.state === "idle" && !fetcher2.data) {
-      fetcher2.load("/api/wait/6000");
-    }
-    if (fetcher3.state === "idle" && !fetcher3.data) {
-      fetcher3.load("/api/wait/8000");
-    }
-  }, [fetcher, fetcher2, fetcher3]);
+  }, [fetcher]);
 
   return (
     <div className="flex flex-col gap-4 items-stretch">
       <header className="flex flex-col items-center bg-red-100 pb-8">
       <h1 className="text-2xl">Header</h1>
       <div className="flex flex-col items-start">
-        <h5 className="text-xl">Fetchers in header with response times</h5>
         <span className="text-sm text-gray-500">
-          4 seconds: {fetcher.state}
-        </span>
-        <span className="text-sm text-gray-500">
-          6 seconds: {fetcher2.state}
-        </span>
-        <span className="text-sm text-gray-500">
-          8 seconds: {fetcher3.state}
+          Fetcher in header with 4 seconds response time: <span className="font-bold">{fetcher.state}</span>
         </span>
       </div>
       </header>
